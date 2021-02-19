a1f2282be82af60130f54fc941650b21cb147920
nikhil-simha <nikhil.simha@airbnb.com>
2021-02-19 12:57:05 -0800

    adding kryo defs - for cpc sketch

---
+++ spark/src/main/scala/ai/zipline/spark/ZiplineKryoRegistrator.scala
@@ -0,0 +1,58 @@
+package ai.zipline.spark
+
+import com.esotericsoftware.kryo.io.{Input, Output}
+import com.esotericsoftware.kryo.{Kryo, Serializer}
+import com.yahoo.sketches.cpc.CpcSketch
+import org.apache.spark.serializer.KryoRegistrator
+
+class CpcSketchKryoSerializer extends Serializer[CpcSketch] {
+  override def write(kryo: Kryo, output: Output, sketch: CpcSketch): Unit = {
+    val bytes = sketch.toByteArray
+    output.writeInt(bytes.size)
+    output.writeBytes(sketch.toByteArray)
+  }
+
+  override def read(kryo: Kryo, input: Input, `type`: Class[CpcSketch]): CpcSketch = {
+    val size = input.readInt()
+    val bytes = input.readBytes(size)
+    CpcSketch.heapify(bytes)
+  }
+}
+
+class ZiplineKryoRegistrator extends KryoRegistrator {
+  // registering classes tells kryo to not send schema on the wire
+  // helps shuffles and spilling to disk
+  override def registerClasses(kryo: Kryo) {
+    //kryo.setWarnUnregisteredClasses(true)
+    val names = Seq(
+      "org.apache.spark.sql.execution.joins.UnsafeHashedRelation",
+      "org.apache.spark.sql.execution.datasources.InMemoryFileIndex$SerializableFileStatus",
+      "org.apache.spark.sql.execution.datasources.InMemoryFileIndex$SerializableBlockLocation",
+      "org.apache.spark.internal.io.FileCommitProtocol$TaskCommitMessage",
+      "org.apache.spark.sql.execution.datasources.ExecutedWriteSummary",
+      "org.apache.spark.sql.execution.datasources.BasicWriteTaskStats",
+      "org.apache.spark.sql.execution.datasources.WriteTaskResult",
+      "org.apache.spark.sql.execution.datasources.InMemoryFileIndex",
+      "org.apache.spark.sql.types.Metadata",
+      "ai.zipline.spark.KeyWithHash",
+      "java.util.HashMap",
+      "java.util.ArrayList",
+      "org.apache.spark.sql.Row",
+      "org.apache.spark.sql.catalyst.InternalRow",
+      "org.apache.spark.sql.catalyst.expressions.GenericRow",
+      "org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema",
+      "org.apache.spark.sql.types.StructField",
+      "org.apache.spark.sql.types.StructType",
+      "org.apache.spark.sql.types.LongType$", // dollar stands for case objects
+      "org.apache.spark.sql.types.StringType",
+      "org.apache.spark.sql.types.StringType$",
+      "org.apache.spark.sql.types.IntegerType$"
+    )
+    names.foreach { name =>
+      kryo.register(Class.forName(name))
+      kryo.register(Class.forName(s"[L$name;")) // represents array of a type to jvm
+    }
+
+    kryo.register(classOf[CpcSketch], new CpcSketchKryoSerializer())
+  }
+}
