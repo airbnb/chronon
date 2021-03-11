99472051e3465fb5d8307aee208fecbfedf641ec
Varant Zanoyan <vzanoyan@gmail.com>
2021-03-11 11:10:12 -0800

    Update spark/src/main/scala/ai/zipline/spark/DataRange.scala
    Co-authored-by: Nikhil <r.nikhilsimha@gmail.com>

--- spark/src/main/scala/ai/zipline/spark/DataRange.scala
+++ spark/src/main/scala/ai/zipline/spark/DataRange.scala
@@ -63,7 +63,7 @@ case class PartitionRange(start: String, end: String) extends DataRange {
     }
   }
 
-  def genScanQuery(query: Query, table: String, fillPartitionIfAbsent: Boolean = false): String = {
+  def genScanQuery(query: Query, table: String, fillIfAbsent: Map[String, String]): String = {
     val queryOpt = Option(query)
     val wheres =
       whereClauses ++ queryOpt.flatMap(q => Option(q.wheres).map(_.asScala)).getOrElse(Seq.empty[String])
