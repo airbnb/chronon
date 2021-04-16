7f66889237448c34dadadf998ed51b733b90eef5
Cristian Figueroa <cristian.figueroa@airbnb.com>
2021-04-16 10:25:04 -0700

    [Zipline][PyAPI] Package

---
+++ api/py/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open("requirements/base.in", "r") as infile:
+    basic_requirements = [line for line in infile]
+
+
+__version__ = "0.1.3"
+
+
+setup(
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=['ai/zipline/repo/compile.py'],
+    description="Zipline python API library",
+    include_package_data=True,
+    install_requires=basic_requirements,
+    name="zipline-ai",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    url=None,
+    version=__version__,
+    zip_safe=False,
+)
