d83af859d4a87a0f0d56a38917bf441e1078cc52
Nikhil Simha Raprolu <nikhil.simha@airbnb.com>
2021-09-01 20:09:20 -0700

    rebase

--- spark/src/main/scala/ai/zipline/spark/DataRange.scala
+++ spark/src/main/scala/ai/zipline/spark/DataRange.scala
@@ -81,12 +81,11 @@ case class PartitionRange(start: String, end: String) extends DataRange {
       .map { step => PartitionRange(step.head, step.last) }
       .toSeq
 
-  def partitions: Seq[String] = {
-    if (start > end) return Seq.empty[String]
+  def partitions: Array[String] =
     Stream
       .iterate(start)(Constants.Partition.after)
       .takeWhile(_ <= end)
-  }
+      .toArray
 
   def shift(days: Int): PartitionRange =
     PartitionRange(Constants.Partition.shift(start, days), Constants.Partition.shift(end, days))
