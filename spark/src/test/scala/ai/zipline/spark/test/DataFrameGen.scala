d256d9eea879803abf9eb9c00679a6b68a6abd20
Cristian Figueroa <cristian.figueroa@airbnb.com>
2021-10-13 14:22:37 -0700

    [Test] Data generation + tests

--- spark/src/test/scala/ai/zipline/spark/test/DataFrameGen.scala
+++ spark/src/test/scala/ai/zipline/spark/test/DataFrameGen.scala
@@ -6,7 +6,7 @@ import ai.zipline.api.{Constants, LongType, StringType}
 import ai.zipline.spark.Conversions
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.expressions.GenericRow
-import org.apache.spark.sql.functions.from_unixtime
+import org.apache.spark.sql.functions.{col, from_unixtime, lit, udf, unix_timestamp}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import ai.zipline.spark.Extensions._
 
@@ -35,5 +35,78 @@ object DataFrameGen {
   //  Generates Entity data
   def entities(spark: SparkSession, columns: Seq[Column], count: Int, partitions: Int): DataFrame = {
     gen(spark, columns :+ Column(Constants.PartitionColumn, StringType, partitions), count)
+  }
+
+  /**
+   * Mutations and snapshots generation.
+   * To generate data for mutations we first generate random entities events.
+   * We set these as insert mutations.
+   * Then we take a sample of rows and mutate them with an is_before and after at a mutation_ts after
+   * the mutation and before the end of the day.
+   * Finally process each snapshot with a group by to generate the snapshot data.
+   * This ensures mutations end state and snapshot data is consistent.
+   * @param spark Spark session
+   * @param columns dataset columns without partition column or reversal column.
+   * @param count Number of events to create
+   * @param partitions
+   * @return (SnapshotDf, MutationDf)
+   */
+  def mutations(spark: SparkSession, columns: Seq[Column], count: Int, partitions: Int, mutationProbability: Double,
+                mutationColumnIdx: Int): (DataFrame, DataFrame) = {
+    val mutationColumn = columns(mutationColumnIdx)
+    // Randomly generated some entity data, store them as inserts w/ mutation_ts = ts and partition = dsOf[ts].
+    val generated = gen(spark, columns :+ Column(Constants.TimeColumn, LongType, partitions), count)
+
+    val withInserts = generated
+      .withColumn(Constants.ReversalColumn, lit(false))
+      .withColumn(Constants.MutationTimeColumn, col(Constants.TimeColumn))
+      .withColumn(Constants.PartitionColumn, from_unixtime((generated.col(Constants.TimeColumn) / 1000), Constants.Partition.format))
+      .drop()
+
+    // Sample some of the inserted data and add a mutation time.
+    val randomLerp = udf((t: Long, upper: Long) => (t + math.random * (upper - t)).toLong)
+    val mutatedFromDf = withInserts
+      .sample(mutationProbability)
+      .withColumn(Constants.ReversalColumn, lit(true))
+      .withColumn(Constants.MutationTimeColumn, randomLerp(col(Constants.MutationTimeColumn), unix_timestamp(col(Constants.PartitionColumn), Constants.Partition.format) * 1000 + 86400 * 1000))
+    val realizedData = spark.sparkContext.parallelize(mutatedFromDf.rdd.collect())
+    val realizedFrom = spark.createDataFrame(realizedData, mutatedFromDf.schema)
+
+    // Generate mutation values then store to fix the random value.
+    val randomReplace = udf(() => (math.random * mutationColumn.cardinality).round.toDouble)
+    val mutatedToDf = realizedFrom
+      .withColumn(Constants.ReversalColumn, lit(false))
+      .withColumn(mutationColumn.name, randomReplace())
+    val realizedTo = spark.createDataFrame(spark.sparkContext.parallelize(mutatedToDf.rdd.collect()), mutatedToDf.schema)
+
+    val mutationsDf = withInserts
+      .union(realizedFrom)
+      .union(realizedTo)
+
+    // Generate snapshot data from mutations by doing a self join
+    val seedDf = mutationsDf
+      .filter(s"${Constants.ReversalColumn} = false")
+      .toDF(mutationsDf.columns.map(_ + "_s"):_*)
+    val expandedEventsDf = seedDf.as("sd")
+      .join(
+        mutationsDf.filter(s"${Constants.ReversalColumn} = false").as("mt"),
+        col(s"${Constants.PartitionColumn}") <= col(s"${Constants.PartitionColumn}_s")
+      ).select("mt.*", s"sd.${Constants.PartitionColumn}_s")
+      .drop(Constants.PartitionColumn, Constants.ReversalColumn)
+      .withColumnRenamed(s"${Constants.PartitionColumn}_s", Constants.PartitionColumn)
+      .dropDuplicates()
+
+    // Given the snapshot
+    val aggregator = new SnapshotAggregator(expandedEventsDf.schema, mutationColumn.name, "listing_id")
+    val snapshotRdd = expandedEventsDf.rdd
+      .keyBy(aggregator.aggregatorKey(_))
+      .aggregateByKey(aggregator.init)(aggregator.update, aggregator.merge)
+      .mapValues(aggregator.finalize(_))
+      .map {
+        case (key, value) => aggregator.toRow(key, value)
+      }
+
+    val snapshotDf = spark.createDataFrame(snapshotRdd, aggregator.outputSchema)
+    (snapshotDf, mutationsDf)
   }
 }
