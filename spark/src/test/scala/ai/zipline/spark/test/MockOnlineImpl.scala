b4f6cc5f0e5f41ea6c594b2173fdb49e33a84bfb
Nikhil Simha Raprolu <nikhil.simha@airbnb.com>
2021-10-24 16:19:52 -0700

    compile fix

--- spark/src/test/scala/ai/zipline/spark/test/MockOnlineImpl.scala
+++ spark/src/test/scala/ai/zipline/spark/test/MockOnlineImpl.scala
@@ -1,9 +1,8 @@
 package ai.zipline.spark.test
 
 import java.io.{ByteArrayInputStream, InputStream}
-
-import ai.zipline.api.{Decoder, KVStore, Mutation, OnlineImpl, StructType}
-import ai.zipline.fetcher.{AvroUtils, RowConversions}
+import ai.zipline.api.{Constants, Decoder, KVStore, Mutation, OnlineImpl, StructType}
+import ai.zipline.fetcher.{ArrayRow, AvroUtils, RowConversions}
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.io.{BinaryDecoder, DecoderFactory}
@@ -18,12 +17,14 @@ class MockDecoder(schema: StructType) extends Decoder {
     reader.read(null, decoder)
   }
 
+  val tsIndex = schema.indexWhere(_.name == Constants.TimeColumn)
+
   def decode(bytes: Array[Byte]): Mutation = {
     val avroSchema = AvroUtils.fromZiplineSchema(schema)
     val avroRecord = byteArrayToAvro(bytes, avroSchema)
-    val after: Array[AnyRef] = schema.fields.map(
+    val after: Array[Any] = schema.fields.map(
       f => RowConversions.fromAvroRecord(avroRecord.get(f.name), f.fieldType).asInstanceOf[AnyRef])
-    Mutation(schema, null, after)
+    Mutation(schema, null, new ArrayRow(after, after(tsIndex).asInstanceOf[Long]))
   }
 }
 
