1d6b5fe5d66743f8de27f71a21a92b5c1a0ecc8a
pengyu_hou <3771747+better365@users.noreply.github.com>
2021-11-11 20:05:53 -0800

    clean up

--- api/py/test/sample/group_bys/sample_team/sample_group_by.py
+++ api/py/test/sample/group_bys/sample_team/sample_group_by.py
@@ -1,30 +1,3 @@
-"""
-Sample group by
-
-from staging_queries.sample_team import sample_staging_query
-
-from ai.zipline.shorthand import *
-from ai.zipline.utils import get_staging_query_output_table_name
-
-v1 = GroupBy(
-    name="sample_group_by",
-    table="sample_namespace.{}".format(get_staging_query_output_table_name(sample_staging_query.v1)),
-    model=DataModel.ENTITIES,
-    keys=[("s2CellId", "s2CellId"), ("place_id", "place_id")],
-    start_partition="2021-03-01",
-    aggs=[
-        Agg(column="impressed_unique_count_1d", op=SUM),
-        Agg(column="viewed_unique_count_1d", op=SUM),
-    ],
-    production=False,
-    table_properties={
-        "sample_config_json": \"""{"sample_key": "sample_value"}\""",
-        "description": "sample description"
-    },
-    output_namespace="sample_namespace",
-)
-"""
-
 from staging_queries.sample_team import sample_staging_query
 
 from ai.zipline.api import ttypes as api
--- api/py/test/sample/group_bys/sample_team/sample_non_prod_group_by.py
+++ api/py/test/sample/group_bys/sample_team/sample_non_prod_group_by.py
@@ -1,32 +1,5 @@
-# from ai.zipline.shorthand import *
 from ai.zipline.api import ttypes as api
 from ai.zipline.group_by import GroupBy
-
-"""
-v1 = GroupBy(
-    name="event_source_from_shorthand",
-    table="sample_namespace.sample_table_group_by",
-    model=DataModel.EVENTS,
-    keys=[("group_by_subject", "group_by_expr")],
-    aggs=[
-        Agg(
-            column="event",
-            expression="event_expr",
-            op=SUM,
-            window="7d",
-        ),
-        Agg(
-            column="event",
-            expression="event_expr",
-            op=SUM,
-        ),
-    ],
-    start_partition="2021-04-09",
-    output_namespace="sample_namespace",
-    online=False,
-    production=False,
-)
-"""
 
 v1 = GroupBy(
     sources=[api.Source(
