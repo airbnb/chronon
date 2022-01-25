ac500e3f1e02bd222a9dcfc97d619ef479810520
Patrick Yoon <patrick.yoon@airbnb.com>
2022-01-25 14:23:44 -0800

    wip benchmark tool

--- spark/src/main/scala/ai/zipline/spark/Driver.scala
+++ spark/src/main/scala/ai/zipline/spark/Driver.scala
@@ -1,21 +1,28 @@
 package ai.zipline.spark
 
 import ai.zipline.api
-import ai.zipline.api.Extensions.{GroupByOps, SourceOps}
-import ai.zipline.api.ThriftJsonCodec
+import ai.zipline.api.Extensions.{GroupByOps, MetadataOps, SourceOps}
+import ai.zipline.api.{ThriftJsonCodec, GroupBy => GroupByConf, Join => JoinConf}
 import ai.zipline.online.{Api, Fetcher, MetadataStore}
 import com.google.gson.GsonBuilder
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.thrift.TBase
 import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand}
-
 import java.io.File
-import scala.collection.JavaConverters.mapAsScalaMapConverter
+import java.util.concurrent.Executors
+
+import scala.collection.JavaConversions.mapAsScalaMap
+import scala.collection.JavaConverters.{asScalaBufferConverter, asScalaIteratorConverter, mapAsScalaMapConverter}
 import scala.collection.mutable
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
+import scala.collection.parallel.{ForkJoinTaskSupport, ThreadPoolTaskSupport}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.forkjoin.ForkJoinPool
 import scala.reflect.ClassTag
 import scala.reflect.internal.util.ScalaClassLoader
+import scala.util.{Failure, Success}
+
+import ai.zipline.online.Fetcher.Request
 
 // The mega zipline cli
 object Driver {
@@ -162,6 +169,94 @@ object Driver {
     }
   }
 
+  object BenchmarkCli {
+
+    class Args extends Subcommand("benchmark") with OnlineSubcommand {
+
+      val confPath: ScallopOption[String] =
+        opt[String](required = true, descr = "Path to the Zipline config file")
+      val ds: ScallopOption[String] =
+        opt[String](required = true, descr = "ds to run benchmark")
+      val numKeys: ScallopOption[Int] =
+        opt[Int](required = true, descr = "number of keys to run benchmark")
+      val concurrency: ScallopOption[Int] =
+        opt[Int](required = false, default = Some(5), descr = "number of fetch request running in parallel")
+      val batchSize: ScallopOption[Int] =
+        opt[Int](required = false, default = Some(5), descr = "number of requests in a batch call")
+      val `type`: ScallopOption[String] = choice(Seq("join", "group-by"), descr = "the type of conf to fetch")
+
+    }
+
+    def run(args: Args): Unit = {
+      val session = SparkSessionBuilder.build("zipline benchmark")
+      val fetcher = new Fetcher(args.impl(args.serializableProps).genKvStore)
+      val conf = args.confPath()
+      val (name, keyMaps): (String, Array[Map[String, AnyRef]]) = if (args.`type`() == "join") {
+        println(s"Reading join from file: $conf")
+        val joinConf: JoinConf = ThriftJsonCodec.fromJsonFile[JoinConf](conf, check = true)
+        val joinTable = s"${joinConf.metaData.outputNamespace}.${joinConf.metaData.cleanName}"
+        val leftQuery = joinConf.left.query
+        val keys = joinConf.joinParts.asScala.flatMap {
+          case jp =>
+            val keyColumns = jp.getGroupBy.getKeyColumns.asScala
+            println(keyColumns.mkString(", "))
+            Option(jp.keyMapping) match {
+              case Some(keyMapping) => keyColumns.map { case k: String => keyMapping.map(_.swap)
+                .getOrElse(k, k)
+              }
+              case _ => keyColumns
+            }
+        }
+        println(s"left : ${leftQuery.selects.asScala.filterKeys(k => keys.contains(k))}")
+        val selectClauseStr = leftQuery.selects.asScala.filterKeys(k => keys.contains(k)).map {
+          case (name, expression) => s"($name) as `$name`"
+        }.mkString(",\n     ")
+        val whereClauseStr = s"ds = '${args.ds()}'"
+        val limitClauseStr = s"LIMIT ${args.numKeys()}"
+        val sqlText = s"SELECT $selectClauseStr FROM $joinTable WHERE $whereClauseStr $limitClauseStr"
+        println(s"sqlText: $sqlText")
+        val keyDf = session.sql(sqlText)
+        (joinConf.getMetaData.name, keyDf.collect().map { row => row.getValuesMap[AnyRef](row.schema.fieldNames) })
+      } else {
+        println(s"Reading groupBy from file: $conf")
+        val groupBy = ThriftJsonCodec.fromJsonFile[GroupByConf](conf, true)
+        val keyFields = groupBy.getKeyColumns.asScala
+        println(s"keys: ${keyFields.mkString(", ")}")
+        val groupByName = groupBy.metaData.name
+        val groupByTable = s"${groupBy.metaData.outputNamespace}.${groupBy.metaData.cleanName}_upload"
+        val groupByServingInfo = fetcher.getGroupByServingInfo(groupByName)
+        val keyCodec = groupByServingInfo.keyCodec
+        val sqlText = s"SELECT key_bytes FROM $groupByTable LIMIT ${args.numKeys()}"
+        println(s"sqlText: $sqlText")
+        val keyDf = session.sql(sqlText)
+        (groupByName, keyDf.collect().map { row => keyCodec.decodeMap(row.getAs[Array[Byte]]("key_bytes"))})
+      }
+      val concurrency = args.concurrency()
+      val batchSize = args.batchSize()
+      val pool = Executors.newFixedThreadPool(5)
+      implicit val xc = ExecutionContext.fromExecutorService(pool)
+      val chunkSize = keyMaps.size / concurrency
+      println(s"starting to fetch $name: ${keyMaps.size} records, with concurrency $concurrency")
+
+      val chunks = keyMaps.grouped(chunkSize).toParArray
+      chunks.tasksupport = new ThreadPoolTaskSupport()
+
+      chunks.map { chunk =>
+        val requests = chunk.sliding(batchSize, batchSize).map(_.map(Request(name, _))).toArray
+        val responseFutures: Array[Future[Seq[Fetcher.Response]]] = if (args.`type`() == "group-by") {
+          requests.map(fetcher.fetchGroupBys(_))
+        } else {
+          requests.map(fetcher.fetchJoin(_))
+        }
+        responseFutures.map { f =>
+          Await.result(f, Duration.Inf)
+        }
+        println(s"processed ${requests.size} requests")
+      }
+      println(s"processed ${chunks.size} chunks")
+    }
+  }
+
   object MetadataUploader {
     class Args extends Subcommand("metadata-upload") with OnlineSubcommand {
       val confPath: ScallopOption[String] =
@@ -254,6 +349,8 @@ object Driver {
     addSubcommand(MetadataUploaderArgs)
     object GroupByStreamingArgs extends GroupByStreaming.Args
     addSubcommand(GroupByStreamingArgs)
+    object BenchmarkCliArgs extends BenchmarkCli.Args
+    addSubcommand(BenchmarkCliArgs)
     requireSubcommand()
     verify()
   }
@@ -283,6 +380,7 @@ object Driver {
           }
           case args.MetadataUploaderArgs => MetadataUploader.run(args.MetadataUploaderArgs)
           case args.FetcherCliArgs       => FetcherCli.run(args.FetcherCliArgs)
+          case args.BenchmarkCliArgs     => BenchmarkCli.run(args.BenchmarkCliArgs)
           case _                         => println(s"Unknown subcommand: ${x}")
         }
       case None => println(s"specify a subcommand please")
