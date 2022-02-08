37e79de1d4ee69f4193e0e4e49b7bfe83992922d
pengyu_hou <3771747+better365@users.noreply.github.com>
2022-02-08 11:34:35 -0800

    sanity check for scala response

--- online/src/main/java/ai/zipline/online/JavaResponse.java
+++ online/src/main/java/ai/zipline/online/JavaResponse.java
@@ -1,14 +1,22 @@
 package ai.zipline.online;
 
+import avro.shaded.com.google.common.collect.ImmutableMap;
+import scala.collection.JavaConverters;
+
 import java.util.Map;
-import scala.collection.JavaConverters;;
+
+;
 
 public class JavaResponse {
-  public JavaRequest request;
-  public Map<String, Object> values;
+    public JavaRequest request;
+    public Map<String, Object> values;
 
-  public JavaResponse(Fetcher.Response scalaResponse) {
-    this.request = new JavaRequest(scalaResponse.request());
-    this.values = JavaConverters.mapAsJavaMapConverter(scalaResponse.values()).asJava();
-  }
+    public JavaResponse(Fetcher.Response scalaResponse) {
+        this.request = new JavaRequest(scalaResponse.request());
+        if (scalaResponse.values() == null || scalaResponse.values().isEmpty()) {
+            this.values = ImmutableMap.of();
+        } else {
+            this.values = JavaConverters.mapAsJavaMapConverter(scalaResponse.values()).asJava();
+        }
+    }
 }
