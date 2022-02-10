1a326c56d80d218ceb1cbbaac5919097aabfe912
Pengyu Hou <3771747+better365@users.noreply.github.com>
2022-02-10 10:05:16 -0800

    NPE handling for Java Response  (#155)
    * sanity check for scala response
    
    * clean up
    
    * reflect nulls
    
    * use java collection map

--- online/src/main/java/ai/zipline/online/JavaResponse.java
+++ online/src/main/java/ai/zipline/online/JavaResponse.java
@@ -1,14 +1,21 @@
 package ai.zipline.online;
 
+import scala.collection.JavaConverters;
+
+import java.util.Collections;
 import java.util.Map;
-import scala.collection.JavaConverters;;
+
 
 public class JavaResponse {
-  public JavaRequest request;
-  public Map<String, Object> values;
+    public JavaRequest request;
+    public Map<String, Object> values;
 
-  public JavaResponse(Fetcher.Response scalaResponse) {
-    this.request = new JavaRequest(scalaResponse.request());
-    this.values = JavaConverters.mapAsJavaMapConverter(scalaResponse.values()).asJava();
-  }
+    public JavaResponse(Fetcher.Response scalaResponse) {
+        this.request = new JavaRequest(scalaResponse.request());
+        if (scalaResponse.values() == null || scalaResponse.values().isEmpty()) {
+            this.values = Collections.emptyMap();
+        } else {
+            this.values = JavaConverters.mapAsJavaMapConverter(scalaResponse.values()).asJava();
+        }
+    }
 }
