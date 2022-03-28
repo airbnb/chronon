adc29f7ec2b6d4c6609f608a31282787b2864e52
Cristian Figueroa <cristian.figueroa@airbnb.com>
2022-03-28 13:42:11 -0700

    Distinct Tokens only

--- spark/src/test/scala/ai/zipline/spark/test/TableUtilsTest.scala
+++ spark/src/test/scala/ai/zipline/spark/test/TableUtilsTest.scala
@@ -19,7 +19,7 @@ class TableUtilsTest {
         |  COALESCE(IF(column_d, column_e, NULL), column_e) AS not_this_one_either,
         |  column_nested.first.second AS not_this_one_as_well
         |FROM fake_table
-        |WHERE column_f IS NOT NULL AND column_g != 'Something'
+        |WHERE column_f IS NOT NULL AND column_g != 'Something' AND column_d > 0
         |""".stripMargin
 
     val columns = tableUtils.getColumnsFromQuery(sampleSql)
