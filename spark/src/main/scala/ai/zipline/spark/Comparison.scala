f6b6494252e3c33a44e424678227d4bd7c242a28
Nikhil Simha Raprolu <nikhil.simha@airbnb.com>
2022-04-18 13:11:28 -0700

    fix test

--- spark/src/main/scala/ai/zipline/spark/Comparison.scala
+++ spark/src/main/scala/ai/zipline/spark/Comparison.scala
@@ -2,7 +2,7 @@ package ai.zipline.spark
 
 import com.google.gson.Gson
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{DoubleType, MapType}
+import org.apache.spark.sql.types.{DoubleType, FloatType, MapType}
 
 import java.util
 
@@ -62,21 +62,22 @@ object Comparison {
           List(finalDf(s"${aName}_$col"), finalDf(s"${bName}_$col"))
         }
     // double columns need to be compared approximately
-    val doubleCols = a.schema.fields.filter(_.dataType == DoubleType).map(_.name).toSet
-    finalDf = finalDf.filter(
-      s"${comparisonColumns
-        .flatMap { col =>
-          val left = s"${aName}_$col"
-          val right = s"${bName}_$col"
-          val compareExpression =
-            if (doubleCols.contains(col)) {
-              s"($left is NOT NULL) AND ($right is NOT NULL) and (abs($left - $right) > 0.00001)"
-            } else { s"($left <> $right)" }
-          Seq(s"(($left IS NULL AND $right IS NOT NULL) OR ($right IS NULL AND $left IS NOT NULL) OR $compareExpression)")
-        }
-        .mkString(" or ")}"
-    )
-    finalDf
+    val doubleCols = a.schema.fields
+      .filter(field => field.dataType == DoubleType || field.dataType == FloatType)
+      .map(_.name)
+      .toSet
+    val comparisonFilters = comparisonColumns
+      .flatMap { col =>
+        val left = s"${aName}_$col"
+        val right = s"${bName}_$col"
+        val compareExpression =
+          if (doubleCols.contains(col)) {
+            s"($left is NOT NULL) AND ($right is NOT NULL) and (abs($left - $right) > 0.00001)"
+          } else { s"($left <> $right)" }
+        Seq(s"(($left IS NULL AND $right IS NOT NULL) OR ($right IS NULL AND $left IS NOT NULL) OR $compareExpression)")
+      }
+    println(s"Using comparison filter:\n  ${comparisonFilters.mkString("\n  ")}")
+    finalDf.filter(comparisonFilters.mkString(" or "))
   }
 
   private def prefixColumnName(df: DataFrame, prefix: String): DataFrame = {
