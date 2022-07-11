af128c1cd94877ccb62818a57d72fe24adfcc20f
Cristian Figueroa <cristian.figueroa@airbnb.com>
2022-07-11 11:03:28 -0700

    Test

--- spark/src/test/scala/ai/chronon/spark/test/GroupByUploadTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/GroupByUploadTest.scala
@@ -42,4 +42,47 @@ class GroupByUploadTest {
       )
     GroupByUpload.run(groupByConf, endDs = yesterday)
   }
+
+  @Test
+  def structSupportTest(): Unit = {
+    val today = Constants.Partition.at(System.currentTimeMillis())
+    val yesterday = Constants.Partition.before(today)
+    tableUtils.sql(s"CREATE DATABASE IF NOT EXISTS $namespace")
+    tableUtils.sql(s"USE $namespace")
+    val eventsBase = "events_source"
+    val eventsTable = "events_table_struct"
+    val eventSchema = List(
+      Column("user", StringType, 10),
+      Column("event1", IntType, 100),
+      Column("event2", DoubleType, 100),
+      Column("event3", LongType, 100),
+      Column("event4", StringType, 100)
+    )
+    val eventBaseDf = DataFrameGen.events(spark, eventSchema, count = 1000, partitions = 18)
+    eventBaseDf.save(s"$namespace.$eventsBase")
+
+    val eventDf = spark.sql(
+      s"""
+      SELECT
+        user
+        , ts
+        , ds
+        , NAMED_STRUCT('event1', event1, 'event2', event2, 'nested', NAMED_STRUCT('event3', event3, 'event4', event4)) as event_struct
+      FROM $namespace.$eventsBase
+      """)
+    eventDf.save(s"$namespace.$eventsTable")
+    val aggregations: Seq[Aggregation] = Seq(
+      Builders.Aggregation(Operation.LAST_K, "event_struct", Seq(WindowUtils.Unbounded), argMap = Map("k" -> "30"))
+    )
+    val keys = Seq("user").toArray
+    val groupByConf =
+      Builders.GroupBy(
+        sources = Seq(Builders.Source.events(Builders.Query(), table = eventsTable)),
+        keyColumns = keys,
+        aggregations = aggregations,
+        metaData = Builders.MetaData(namespace = namespace, name = "test_last_k_upload"),
+        accuracy = Accuracy.TEMPORAL
+      )
+    GroupByUpload.run(groupByConf, endDs = yesterday)
+  }
 }
