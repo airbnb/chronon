0cf57d2f4b9afee78a777f10ece9ac4f25f97f79
Cristian Figueroa <cristian.figueroa@airbnb.com>
2022-09-21 17:35:15 -0700

    [WIP] Stats Summary for DF

--- spark/src/main/scala/ai/chronon/spark/stats/StatsCompute.scala
+++ spark/src/main/scala/ai/chronon/spark/stats/StatsCompute.scala
@@ -1,33 +1,82 @@
 package ai.chronon.spark.stats
 
-import java.util
+import ai.chronon.aggregator.row.RowAggregator
+import ai.chronon.api.Extensions.WindowUtils
 import ai.chronon.api._
-import ai.chronon.spark.PartitionRange
-import org.apache.spark.sql.{Column, DataFrame}
+import ai.chronon.spark.Conversions
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.{Column, DataFrame, SparkSession, functions, Row => SparkRow}
+
+import scala.collection.JavaConverters._
 
 object StatsGenerator {
-  case class MetricTransform(name: String,
-                             operation: Operation,
-                             argMap: util.Map[String, String] = null,
-                             additionalExprs: Seq[(String, String)] = null)
+  /*
+   * Base definition for stats metrics.
+   */
+  case class MetricTransform(name: String, expression: Column, operation: Operation)
 
-  val numericAggregations = Seq(MetricTransform())
+  /*
+   * Stats applied to any column
+   */
+  def anyTransforms(column: Column): Seq[MetricTransform] = Seq(
+      MetricTransform(s"null_count_$column", column.isNull, operation = Operation.SUM)
+    // TODO: Figure out serialization issue.
+    //, MetricTransform(s"approx_unique_count_$column", column, operation = Operation.APPROX_UNIQUE_COUNT)
+  )
 
-  class StatsAggregator {
-    def init = ???
-    def update = ???
-    def merge = ???
+  /*
+   * Stats applied to numeric columns
+   */
+  def numericTransforms(column: Column): Seq[MetricTransform] = anyTransforms(column) ++ Seq(
+    //MetricTransform(s"tdigest_$column", column, operation = Operation.APPROX_PERCENTILE)
+    // T-Digest placeholder MetricTransform...
+  )
+
+  def buildMetrics(fields: Array[(String, DataType)]): Seq[MetricTransform] = {
+    val metrics = fields.flatMap {
+      case (name, dataType) =>
+        if (DataType.isNumeric(dataType)) {
+          numericTransforms(functions.col(name))
+        } else {
+          anyTransforms(functions.col(name))
+        }
+    }
+    metrics :+ MetricTransform("total", lit(true), Operation.COUNT)
   }
+
+  def buildAggregator(metrics: Seq[MetricTransform], inputDf: DataFrame): RowAggregator = {
+    val schema = Conversions.toChrononSchema(inputDf.schema)
+    val aggParts = metrics.flatMap { m =>
+      def buildAggPart(name: String): AggregationPart = {
+        val aggPart = new AggregationPart()
+        aggPart.setInputColumn(name)
+        aggPart.setOperation(m.operation)
+        aggPart.setWindow(WindowUtils.Unbounded)
+        aggPart
+      }
+      Seq(buildAggPart(m.name))
+    }
+    new RowAggregator(schema, aggParts)
+  }
+
   /*
    * Navigate the dataframe and compute the statistics.
    */
-  def summary(df: DataFrame, keys: Seq[String], partitionRange: PartitionRange): DataFrame = {
-    val aggregator = new StatsAggregator
-    df
-      .select(df.columns.filter(!keys.contains(_)).map(colName => new Column(colName)): _*) // Remove the keys.
+  def summary(inputDf: DataFrame, keys: Seq[String], sparkSession: SparkSession): Array[Any] = {
+    val noKeysDf = inputDf.select(inputDf.columns.filter(colName => !keys.contains(colName))
+      .map(colName => new Column(colName)): _*)
+    val metrics = buildMetrics(Conversions.toChrononSchema(noKeysDf.schema))
+    val selectedDf = noKeysDf.select(metrics.map(m => m.expression): _*).toDF(metrics.map(m => m.name): _*)
+    selectedDf.printSchema()
+    val aggregator = buildAggregator(metrics, selectedDf)
+    val result = selectedDf
       .rdd
-      .treeAggregate(aggregator.init)(
-        seqOp = aggregator.update, combOp = aggregator.merge
-      )
+      .map(Conversions.toChrononRow(_, -1))
+      .treeAggregate(aggregator.init)(seqOp = aggregator.updateWithReturn, combOp = aggregator.merge)
+
+    result
+    // Having the result of the aggregations we could do post process to return null rates for example.
+    //sparkSession.createDataFrame(List(new GenericRow(resultRdd)).map(a => a.asInstanceOf[SparkRow]).asJava, Conversions.fromChrononSchema(aggregator.outputSchema))
   }
 }
