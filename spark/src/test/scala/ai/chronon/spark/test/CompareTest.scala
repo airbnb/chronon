ab370ebdc81f5d2727fdda509c1bcefe624574ae
vamsee_yarlagadda <vamsee.y@airbnb.com>
2022-09-26 22:47:28 -0500

    Baseline - 2

---
+++ spark/src/test/scala/ai/chronon/spark/test/CompareTest.scala
@@ -0,0 +1,33 @@
+package ai.chronon.spark.test
+
+import ai.chronon.spark.stats.StatsGenerator
+import ai.chronon.spark.{SparkSessionBuilder, TableUtils}
+import com.google.gson.Gson
+import org.apache.spark.sql.SparkSession
+import org.junit.Test
+
+class CompareTest {
+  lazy val spark: SparkSession = SparkSessionBuilder.build("StatsComputeTest", local = true)
+
+  private val tableUtils = TableUtils(spark)
+
+  @Test
+  def basicTest(): Unit = {
+    val data = Seq(
+      (1, Some(1), 1.0, "a"),
+      (1, Some(2), 2.0, "b"),
+      (2, Some(2), 2.0, "c"),
+      (3, None, 1.0, "d")
+    )
+    val columns = Seq("keyId", "value", "double_value", "string_value")
+    val rdd = spark.sparkContext.parallelize(data)
+    val df = spark.createDataFrame(rdd).toDF(columns:_*)
+    df.show()
+    val result = StatsGenerator.summary(df, Seq("keyId"), spark)
+    println(result)
+    val gson = new Gson()
+    println(gson.toJson(result))
+    //result.show()
+  }
+
+}
