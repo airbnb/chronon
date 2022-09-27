bf81e17ed4b292ba0cc2b12d96e0587a18821ff5
Cristian Figueroa <cristian.figueroa@airbnb.com>
2022-09-27 10:20:43 -0700

    Make example with custom k

--- api/py/test/sample/group_bys/sample_team/event_sample_group_by.py
+++ api/py/test/sample/group_bys/sample_team/event_sample_group_by.py
@@ -23,7 +23,7 @@ v1 = GroupBy(
         ),
         Aggregation(
             input_column="event",
-            operation=Operation.APPROX_PERCENTILE([0.99, 0.95, 0.5]), # p99, p95, Median
+            operation=Operation.APPROX_PERCENTILE([0.99, 0.95, 0.5], k=200), # p99, p95, Median
         )
     ],
     online=True,
