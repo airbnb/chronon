820b1da11859f4e88cae4046e57f40ed56e47425
vamsee_yarlagadda <vamsee.y@airbnb.com>
2022-09-28 16:38:46 -0500

    [Stats] POC of compare function

--- spark/src/main/scala/ai/chronon/spark/stats/CompareJob.scala
+++ spark/src/main/scala/ai/chronon/spark/stats/CompareJob.scala
@@ -2,118 +2,67 @@ package ai.chronon.spark.stats
 
 import ai.chronon
 import ai.chronon.api.Extensions._
+import ai.chronon.spark.Extensions._
 import ai.chronon.api._
 import ai.chronon.online._
-import ai.chronon.spark.{PartitionRange, TableUtils}
-import org.apache.spark.sql.SparkSession
+import ai.chronon.spark.{Conversions, PartitionRange, TableUtils}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import java.util
-import scala.collection.JavaConverters._
+object CompareJob {
+  /*
+  * Navigate the dataframes and compare them and fetch statistics.
+  */
+  def compare(leftDf: DataFrame, rightDf: DataFrame, keys: Seq[String], mapping: Map[String, String] = Map.empty): DataMetrics = {
+    // 1. Check for schema consistency issues
+    // TODO: Check for datatypes, number of arguments, mapping to match the cols on each side
+    // TODO: Mapping should either be empty or filled out completely no partial mapping is allowed.
 
-class CompareJob(session: SparkSession, joinConf: Join, endDate: String, impl: Api) extends Serializable {
+    // 2. Build comparison dataframe
+    // TODO: Should consider the partition column as a separate thing from keys
+    println(s"""Join keys: ${keys.mkString(", ")}
+        |Left Schema:
+        |${leftDf.schema.pretty}
+        |
+        |Right Schema:
+        |${rightDf.schema.pretty}
+        |
+        |""".stripMargin)
 
-  val kvStore: KVStore = impl.genKvStore
-  val metadataStore = new MetadataStore(kvStore, timeoutMillis = 10000)
-  val fetcher: Fetcher = impl.fetcher
-  val joinCodec = fetcher.getJoinCodecs(joinConf.metaData.nameToFilePath).get
-  val rawTable: String = impl.logTable
-  val tblProperties = Option(joinConf.metaData.tableProperties)
-    .map(_.asScala.toMap)
-    .getOrElse(Map.empty[String, String])
-  val tableUtils: TableUtils = TableUtils(session)
+    // Rename the left data source columns with a suffix (except the keys) to reduce the ambiguity
+    val renamedLeftDf = leftDf.schema.fieldNames.foldLeft(leftDf)((df, field) => {
+      if (!keys.contains(field)) {
+        df.withColumnRenamed(field, s"${field}${CompareMetrics.leftSuffix}")
+      } else {
+        df
+      }
+    })
 
-  private def unfilledRange(inputTable: String, outputTable: String): Option[PartitionRange] = {
-    val joinName = joinConf.metaData.nameToFilePath
-    val inputPartitions = session.sqlContext
-      .sql(
-        s"""
-           |select distinct ${Constants.PartitionColumn}
-           |from $inputTable
-           |where name = '$joinName' """.stripMargin)
-      .collect()
-      .map(row => row.getString(0))
-      .toSet
+    // 3. Join both the dataframes based on the keys and the partition column
+    renamedLeftDf.validateJoinKeys(rightDf, keys)
+    val joinedDf = renamedLeftDf.join(rightDf, keys, "full")
 
-    val inputStart = inputPartitions.reduceOption(Ordering[String].min)
-    assert(inputStart.isDefined,
-        s"""
-           |The join name $joinName does not have available logged data yet.
-           |Please double check your logging status""".stripMargin)
+    // Rename the right data source columns with a suffix (except the keys) to reduce the ambiguity
+    val compareDf = rightDf.schema.fieldNames.foldLeft(joinedDf)((df, field) => {
+      if (!keys.contains(field)) {
+        df.withColumnRenamed(field, s"${field}${CompareMetrics.rightSuffix}")
+      } else {
+        df
+      }
+    })
 
-    val fillablePartitions = PartitionRange(inputStart.get, endDate).partitions.toSet
-    val outputMissing = fillablePartitions -- tableUtils.partitions(outputTable)
-    val inputMissing = fillablePartitions -- inputPartitions
-    val missingPartitions = outputMissing -- inputMissing
+    val leftChrononSchema = StructType(
+      "input",
+      Conversions.toChrononSchema(leftDf.schema)
+        .filterNot(tup => keys.contains(tup._1))
+        .map(tup => StructField(tup._1, tup._2)))
 
-    println(
-      s"""
-         |   Unfilled range computation:
-         |   Output table: $outputTable
-         |   Missing output partitions: $outputMissing
-         |   Missing input partitions: $inputMissing
-         |   Unfilled Partitions: $missingPartitions
-         |""".stripMargin)
-    if (missingPartitions.isEmpty) {
-      println(
-        s"$outputTable seems to be caught up - to either " +
-          s"$inputTable(latest ${tableUtils.lastAvailablePartition(inputTable)}) or $endDate.")
-      return None
-    }
-    Some(PartitionRange(missingPartitions.min, missingPartitions.max))
-  }
+    // 4. Run the consistency check
+    val (df, metrics) = CompareMetrics.compute(leftChrononSchema.fields, compareDf)
 
-  private def buildLogTable(): Unit = {
-    val unfilled = unfilledRange(rawTable, joinConf.metaData.loggedTable)
-    if (unfilled.isEmpty) return
-    val joinName = joinConf.metaData.nameToFilePath
-    val rawTableScan = unfilled.get.genScanQuery(null, rawTable)
-    val rawDf = tableUtils.sql(rawTableScan).where(s"name = '$joinName'")
-    println(s"scanned data for $joinName")
-    val outputSize = joinCodec.outputFields.length
-    tableUtils.insertPartitions(CompareJob.flattenKeyValueBytes(rawDf, joinCodec, outputSize),
-                                joinConf.metaData.loggedTable, tableProperties = tblProperties)
-  }
+    // 5. Optionally save the compare results to a table
+    // TODO Save the results to a table
+    // df.withTimeBasedColumn("ds").save(joinConf.metaData.consistencyTable, tableProperties = tblProperties)
 
-  // replace join's left side with the logged table
-  private def buildComparisonJoin(): Join = {
-    val copiedJoin = joinConf.deepCopy()
-    val loggedSource: Source = new Source()
-    val loggedEvents: EventSource = new EventSource()
-    val query = new Query()
-    val mapping = joinCodec.keyFields.map(_.name).map(k => k -> k) ++
-      joinCodec.valueFields.map(_.name).map(v => s"$v${CompareMetrics.loggedSuffix}" -> v) ++
-      joinCodec.timeFields.map(_.name).map(t => t -> t)
-    val selects = new util.HashMap[String, String]()
-    mapping.foreach { case (key, value) => selects.put(key, value) }
-    query.setSelects(selects)
-    loggedEvents.setQuery(query)
-    loggedEvents.setTable(joinConf.metaData.loggedTable)
-    loggedSource.setEvents(loggedEvents)
-    copiedJoin.setLeft(loggedSource)
-    val newName = joinConf.metaData.comparisonConfName
-    copiedJoin.metaData.setName(newName)
-    copiedJoin
-  }
-
-  private def buildComparisonTable(): Unit = {
-    val unfilled = tableUtils.unfilledRange(joinConf.metaData.comparisonTable, PartitionRange(null, endDate), Some(joinConf.metaData.loggedTable))
-    if (unfilled.isEmpty) return
-    val join = new chronon.spark.Join(buildComparisonJoin(), unfilled.get.end, TableUtils(session))
-    join.computeJoin(Some(30))
-  }
-
-  def buildConsistencyMetrics(): DataMetrics = {
-    buildLogTable()
-    buildComparisonTable()
-    val unfilled = tableUtils.unfilledRange(joinConf.metaData.consistencyTable, PartitionRange(null, endDate), Some(joinConf.metaData.comparisonTable))
-    if (unfilled.isEmpty) return null
-    val comparisonDf = tableUtils.sql(unfilled.get.genScanQuery(null, joinConf.metaData.comparisonTable))
-    val renamedDf =
-      joinCodec.valueFields.foldLeft(comparisonDf)((df, field) =>
-        df.withColumnRenamed(field.name, s"${field.name}${CompareMetrics.backfilledSuffix}"))
-    val (df, metrics) = CompareMetrics.compute(joinCodec.valueFields, renamedDf)
-    df.withTimeBasedColumn("ds").save(joinConf.metaData.consistencyTable, tableProperties = tblProperties)
-    metadataStore.putConsistencyMetrics(joinConf, metrics)
     metrics
   }
 }
--- spark/src/test/scala/ai/chronon/spark/test/CompareTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/CompareTest.scala
@@ -1,29 +1,45 @@
 package ai.chronon.spark.test
 
-import ai.chronon.spark.stats.StatsGenerator
+import ai.chronon.aggregator.windowing.TsUtils
+import ai.chronon.spark.stats.CompareJob
 import ai.chronon.spark.{SparkSessionBuilder, TableUtils}
 import com.google.gson.Gson
 import org.apache.spark.sql.SparkSession
 import org.junit.Test
 
 class CompareTest {
-  lazy val spark: SparkSession = SparkSessionBuilder.build("StatsComputeTest", local = true)
+  lazy val spark: SparkSession = SparkSessionBuilder.build("CompareTest", local = true)
 
   private val tableUtils = TableUtils(spark)
 
+  def toTs(arg: String): Long = TsUtils.datetimeToTs(arg)
+
   @Test
   def basicTest(): Unit = {
-    val data = Seq(
-      (1, Some(1), 1.0, "a"),
-      (1, Some(2), 2.0, "b"),
-      (2, Some(2), 2.0, "c"),
-      (3, None, 1.0, "d")
+    val leftData = Seq(
+      (1, Some(1), 1.0, "a", toTs("2021-04-10 09:00:00"), "2021-04-10"),
+      (1, Some(2), 2.0, "b", toTs("2021-04-10 11:00:00"), "2021-04-10"),
+      (2, Some(2), 2.0, "c", toTs("2021-04-10 15:00:00"), "2021-04-10"),
+      (3, None, 1.0, "d", toTs("2021-04-10 19:00:00"), "2021-04-10")
     )
-    val columns = Seq("keyId", "value", "double_value", "string_value")
-    val rdd = spark.sparkContext.parallelize(data)
-    val df = spark.createDataFrame(rdd).toDF(columns:_*)
-    df.show()
-    val result = StatsGenerator.summary(df, Seq("keyId"), spark)
+
+    val rightData = Seq(
+      (1, Some(1), 5.0, "a", toTs("2021-04-10 09:00:00"), "2021-04-10"),
+      (2, Some(3), 2.0, "b", toTs("2021-04-10 11:00:00"), "2021-04-10"),
+      (2, Some(5), 6.0, "c", toTs("2021-04-10 15:00:00"), "2021-04-10"),
+      (3, None, 1.0, "d", toTs("2021-04-10 19:00:00"), "2021-04-10")
+    )
+
+    val columns = Seq("serial", "value", "rating", "keyId", "ts", "ds")
+    val leftRdd = spark.sparkContext.parallelize(leftData)
+    val leftDf = spark.createDataFrame(leftRdd).toDF(columns:_*)
+    val rightRdd = spark.sparkContext.parallelize(rightData)
+    val rightDf = spark.createDataFrame(rightRdd).toDF(columns:_*)
+
+    leftDf.show()
+    rightDf.show()
+
+    val result = CompareJob.compare(leftDf, rightDf, Seq("keyId", "ts", "ds"))
     println(result)
     val gson = new Gson()
     println(gson.toJson(result))
