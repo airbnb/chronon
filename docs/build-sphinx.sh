459cc1943b388d1b958acd6ca5a6ed0d738ac392
vamsee_yarlagadda <vamsee.y@airbnb.com>
2022-10-19 20:13:49 -0500

    SBT trigger to build sphinx docs

---
+++ docs/build-sphinx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+BUILD_DIR=docs/build
+VIRTUAL_ENV=${BUILD_DIR}/sphinx
+
+# Cleanup old artifacts
+rm -rf ${BUILD_DIR}
+
+# Setup Virtualenv for Sphinx with all its dependencies
+virtualenv ${VIRTUAL_ENV}
+source ${VIRTUAL_ENV}/bin/activate
+pip install -r docs/sphinx-requirements.txt
+
+# Install the repo's Chronon python API
+python -m build api/py
+pip install api/py/dist/chronon-ai*.tar.gz
+
+# Run the Sphinx build
+${VIRTUAL_ENV}/bin/sphinx-build -b html docs/source/ ${BUILD_DIR}/html
+
+deactivate
