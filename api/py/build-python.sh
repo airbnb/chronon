9e78d7f052bd98989514b8a596564252852ba811
vamsee_yarlagadda <vamsee.y@airbnb.com>
2022-11-21 11:50:03 -0600

    Modified script to support python build actions

--- api/py/build-python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install the repo's Chronon python API
-export CHRONON_PY_VERSION=$1
-echo "-------------------------------------- $2 -----------------------------------"
-#python -m build api/py
--- api/py/release.sh
+++ api/py/release.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-if [[ -z "$1" ]] ; then
-  echo "Please provide PyPi repository to upload to."
-  exit 1
-fi
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ "$BRANCH" != "master" ]]; then
-  echo "$(tput bold) You are not on master!"
-  echo "$(tput sgr0) Are you sure you want to release? (y to continue)"
-  read response
-  if [[ "$response" != "y" ]]; then
-    echo "Not releasing then. Wise choice."
-    exit 0
-  fi
-fi
 set -o xtrace
-echo "Releasing to PyPi repository: $1"
+
+export CHRONON_PY_VERSION=$1
+ACTION=$2
+
 echo "Finding working directory.."
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Removing old distributions..."
-rm $SCRIPT_DIR/dist/*
-echo "Compiling thrift files..."
-thrift --gen py -out $SCRIPT_DIR/ai/chronon $SCRIPT_DIR/../thrift/api.thrift
-echo "Running tests..."
-cd $SCRIPT_DIR && tox
-if [[ $? -eq 0 ]]; then
-  echo "Checking for straggling files."
-  git add .
-  git diff-index --cached --quiet HEAD
+rm -f $SCRIPT_DIR/dist/*
+
+# The default action is "build"
+if [[ -z "${ACTION}" ]] || [[ "${ACTION}" == "build" ]]; then
+  PYPI_REPOSITORY="internal"
+  echo "Running build..."
+  cd $SCRIPT_DIR
+  set -e
+  python -m build
+elif [[ "${ACTION}" == "release" ]]; then
+  PYPI_REPOSITORY="chronon-pypi"
+  # Check git state, Run Tests, Build, Release
+  echo "Running tests, git check, build and release..."
+  cd $SCRIPT_DIR && tox
   if [[ $? -eq 0 ]]; then
-    echo "Building"
-    python -m build
-    echo "Running upload."
-    python -m twine upload --repository $1 dist/*
+    echo "Checking for straggling files."
+    git add .
+    git diff-index --cached --quiet HEAD
+    if [[ $? -eq 0 ]]; then
+      set -e
+      echo "Building"
+      python -m build
+      echo "Releasing to PyPi repository: ${PYPI_REPOSITORY}"
+      python -m twine upload --repository ${PYPI_REPOSITORY} dist/*
+    else
+      echo "Found straggling git files. Run git status for details."
+      exit 1
+    fi
   else
-    echo "Found straggling git files. Run git status for details."
-    exit 1
+    echo "Failed test. Aborting upload."
   fi
 else
-  echo "Failed test. Aborting upload."
+  echo "Invalid action for Python API: ${ACTION}. Please select either [build, release]"
+  exit 1
 fi
