1cd91eb5e439bfdf4ca8107e97e22958dba67f5e
Pengyu <3771747+better365@users.noreply.github.com>
2022-12-16 10:18:43 -0800

    drop external cols for OOC

--- spark/src/main/scala/ai/chronon/spark/stats/ConsistencyJob.scala
+++ spark/src/main/scala/ai/chronon/spark/stats/ConsistencyJob.scala
@@ -60,6 +60,18 @@ class ConsistencyJob(session: SparkSession, joinConf: Join, endDate: String) ext
     val allMetrics = unfilledRanges.map { unfilled =>
       val comparisonDf = tableUtils.sql(unfilled.genScanQuery(null, joinConf.metaData.comparisonTable))
       val loggedDf = tableUtils.sql(unfilled.genScanQuery(null, joinConf.metaData.loggedTable)).drop(Constants.SchemaHash)
+      // external columns are logged during online env, therefore they could not be used for computing OOC
+      val externalCols: Seq[String] = if (!joinConf.onlineExternalParts.isEmpty) {
+        joinConf.onlineExternalParts.asScala.map { part => {
+          val keys = part.source.getKeySchema.params.asScala.map(_.name)
+          val values = part.source.getValueSchema.params.asScala.map(_.name)
+          keys ++ values
+        }
+        }.flatMap(_.toSet)
+      } else Seq.empty
+      println(s"drop external columns ${externalCols.mkString(",")}")
+      loggedDf.drop(externalCols: _*)
+
       println("Starting compare job for stats")
       //TODO: Using timestamp as comparison key is a proxy for row_id as the latter is precise on ts and join key.
       // Using solely timestamp can lead to issues for fetches that involve multiple keys.
