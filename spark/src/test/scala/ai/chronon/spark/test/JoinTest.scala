cd21d25ee59a9de6a594ebc573a9af68d1108f61
hzding621 <hzding621@gmail.com>
2023-02-13 12:31:42 -0800

    add unit test

--- spark/src/test/scala/ai/chronon/spark/test/JoinTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/JoinTest.scala
@@ -996,4 +996,52 @@ class JoinTest {
       table = viewsTable
     )
   }
+
+  @Test
+  def testMigration(): Unit = {
+
+    // Left
+    val itemQueriesTable = s"$namespace.item_queries"
+    val leftStart = Constants.Partition.minus(today, new Window(100, TimeUnit.DAYS))
+    val leftSource = Builders.Source.events(Builders.Query(startPartition = leftStart), table = itemQueriesTable)
+
+    // Right
+    val viewsTable = s"$namespace.view_events"
+    val viewsSource = Builders.Source.events(
+      table = viewsTable,
+      query = Builders.Query(selects = Builders.Selects("time_spent_ms"), startPartition = yearAgo)
+    )
+    val groupBy = Builders.GroupBy(
+      sources = Seq(viewsSource),
+      keyColumns = Seq("item"),
+      aggregations = Seq(
+        Builders.Aggregation(operation = Operation.AVERAGE, inputColumn = "time_spent_ms"),
+        Builders.Aggregation(operation = Operation.MIN, inputColumn = "ts"),
+        Builders.Aggregation(operation = Operation.MAX, inputColumn = "ts")
+      ),
+      metaData = Builders.MetaData(name = "unit_test.item_views", namespace = namespace, team = "chronon"),
+      accuracy = Accuracy.TEMPORAL
+    )
+
+    // Join
+    val join = Builders.Join(
+      left = leftSource,
+      joinParts = Seq(Builders.JoinPart(groupBy = groupBy, prefix = "user")),
+      metaData = Builders.MetaData(name = s"test.join_migration", namespace = namespace, team = "chronon")
+    )
+
+    // test older versions before migration
+    // older versions do not have the bootstrap hash, but should not trigger recompute if no bootstrap_parts
+    val productionHashV1 = Map(
+      "left_source" -> "FF4eqikZnR",
+      "test_namespace_jointest.test_join_migration_user_unit_test_item_views" -> "J1IZNKLzUo"
+    )
+    assertEquals(0, join.tablesToDrop(productionHashV1).length)
+
+    // test newer versions
+    val productionHashV2 = productionHashV1 ++ Map(
+      "test_namespace_jointest.test_join_migration_bootstrap" -> "1B2M2Y8Asg"
+    )
+    assertEquals(0, join.tablesToDrop(productionHashV2).length)
+  }
 }
