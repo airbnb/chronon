8a7fcae440031180e51e43df61f3f3e9c32e0c09
Nikhil Simha Raprolu <nikhil.simha@airbnb.com>
2023-03-19 00:31:22 -0700

    Thrift decoder + catalyst util support for project exec

--- online/src/main/scala/ai/chronon/online/CatalystUtil.scala
+++ online/src/main/scala/ai/chronon/online/CatalystUtil.scala
@@ -4,8 +4,21 @@ import ai.chronon.api.{DataType, StructType}
 import ai.chronon.online.CatalystUtil.IteratorWrapper
 import ai.chronon.online.Extensions.StructTypeOps
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.codegen.CodeGenerator
-import org.apache.spark.sql.execution.{BufferedRowIterator, WholeStageCodegenExec}
+import org.apache.spark.sql.catalyst.expressions.UnsafeProjection
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodeAndComment, CodeGenerator, CodegenContext}
+import org.apache.spark.sql.execution.debug.DebugQuery
+import org.apache.spark.sql.execution.streaming.MemoryStream
+import org.apache.spark.sql.execution.streaming.sources.ContinuousMemoryStream
+import org.apache.spark.sql.execution.{
+  BufferedRowIterator,
+  CodegenSupport,
+  CollapseCodegenStages,
+  InputAdapter,
+  LocalTableScanExec,
+  ProjectExec,
+  WholeStageCodegenExec
+}
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.{SparkSession, types}
 
 import java.lang.ThreadLocal
@@ -24,11 +37,16 @@ object CatalystUtil {
     private val elemArr: mutable.Queue[T] = mutable.Queue.empty[T]
   }
 
-  lazy val session: SparkSession = SparkSession
-    .builder()
-    .appName(s"catalyst_test_${Thread.currentThread().toString}")
-    .master("local[*]")
-    .getOrCreate()
+  lazy val session: SparkSession = {
+    val spark = SparkSession
+      .builder()
+      .appName(s"catalyst_test_${Thread.currentThread().toString}")
+      .master("local[*]")
+      .config("spark.sql.adaptive.enabled", "false")
+      .getOrCreate()
+    assert(spark.sessionState.conf.wholeStageEnabled)
+    spark
+  }
 
 }
 
@@ -43,45 +61,70 @@ class ThreadLocalCatalystUtil(expressions: collection.Seq[(String, String)], inp
 
 // This class by itself it not thread safe because of the transformBuffer
 private class CatalystUtil(expressions: collection.Seq[(String, String)], inputSchema: StructType) {
-  private val selectClauses = expressions.map { case (name, expr) => s"$expr as $name" }.mkString(", ")
-  private val sessionTable = s"q${math.abs(selectClauses.hashCode)}_f${math.abs(inputSparkSchema.pretty.hashCode)}"
-  private val query = s"SELECT $selectClauses FROM $sessionTable"
-  private val iteratorWrapper: IteratorWrapper[InternalRow] = new IteratorWrapper[InternalRow]
-  val (sparkSQLTransformerBuffer: BufferedRowIterator, outputSparkSchema: types.StructType) = {
-    initializeIterator(iteratorWrapper)
-  }
+  private val selectClauses = expressions.map { case (name, expr) => s"$expr as $name" }
+  private val sessionTable =
+    s"q${math.abs(selectClauses.mkString(", ").hashCode)}_f${math.abs(inputSparkSchema.pretty.hashCode)}"
+  private val (transformFunc: (InternalRow => InternalRow), outputSparkSchema: types.StructType) = initialize()
   @transient lazy val outputChrononSchema = SparkConversions.toChrononSchema(outputSparkSchema)
   private val outputDecoder = SparkInternalRowConversions.from(outputSparkSchema)
   @transient lazy val inputSparkSchema = SparkConversions.fromChrononSchema(inputSchema)
-  private val inputEncoder = SparkInternalRowConversions.to(SparkConversions.fromChrononSchema(inputSchema))
+  private val inputEncoder = SparkInternalRowConversions.to(inputSparkSchema)
+  private val inputArrEncoder = SparkInternalRowConversions.to(inputSparkSchema, false)
+  private lazy val outputArrDecoder = SparkInternalRowConversions.from(outputSparkSchema, false)
+
+  def performSql(values: Array[Any]): Array[Any] = {
+    val internalRow = inputArrEncoder(values).asInstanceOf[InternalRow]
+    val resultRow = transformFunc(internalRow)
+    val outputVal = outputArrDecoder(resultRow)
+    Option(outputVal).map(_.asInstanceOf[Array[Any]]).orNull
+  }
 
   def performSql(values: Map[String, Any]): Map[String, Any] = {
     val internalRow = inputEncoder(values).asInstanceOf[InternalRow]
-    iteratorWrapper.put(internalRow)
-    while (sparkSQLTransformerBuffer.hasNext) {
-      val resultInternalRow = sparkSQLTransformerBuffer.next()
-      val outputVal = outputDecoder(resultInternalRow)
-      return Option(outputVal).map(_.asInstanceOf[Map[String, Any]]).orNull
-    }
-    null
+    val resultRow = transformFunc(internalRow)
+    val outputVal = outputDecoder(resultRow)
+    Option(outputVal).map(_.asInstanceOf[Map[String, Any]]).orNull
   }
 
-  private def initializeIterator(
-      iteratorWrapper: IteratorWrapper[InternalRow]): (BufferedRowIterator, types.StructType) = {
+  private def initialize(): (InternalRow => InternalRow, types.StructType) = {
     val session = CatalystUtil.session
+
+    // create dummy df with sql query and schema
     val emptyRowRdd = session.emptyDataFrame.rdd
     val inputSparkSchema = SparkConversions.fromChrononSchema(inputSchema)
     val emptyDf = session.createDataFrame(emptyRowRdd, inputSparkSchema)
     emptyDf.createOrReplaceTempView(sessionTable)
-    val outputSchema = session.sql(query).schema
-    val logicalPlan = session.sessionState.sqlParser.parsePlan(query)
-    val plan = session.sessionState.executePlan(logicalPlan)
-    val codeGenerator = plan.executedPlan.asInstanceOf[WholeStageCodegenExec]
-    val (ctx, cleanedSource) = codeGenerator.doCodeGen()
-    val (clazz, _) = CodeGenerator.compile(cleanedSource)
-    val references = ctx.references.toArray
-    val buffer = clazz.generate(references).asInstanceOf[BufferedRowIterator]
-    buffer.init(0, Array(iteratorWrapper))
-    (buffer, outputSchema)
+    val df = session.sqlContext.table(sessionTable).selectExpr(selectClauses: _*)
+
+    // extract transform function from the df spark plan
+    val func: InternalRow => InternalRow = df.queryExecution.executedPlan match {
+      case whc: WholeStageCodegenExec => {
+        val (ctx, cleanedSource) = whc.doCodeGen()
+        val (clazz, _) = CodeGenerator.compile(cleanedSource)
+        val references = ctx.references.toArray
+        val buffer = clazz.generate(references).asInstanceOf[BufferedRowIterator]
+        val iteratorWrapper: IteratorWrapper[InternalRow] = new IteratorWrapper[InternalRow]
+        buffer.init(0, Array(iteratorWrapper))
+        def codegenFunc(row: InternalRow): InternalRow = {
+          iteratorWrapper.put(row)
+          while (buffer.hasNext) {
+            return buffer.next()
+          }
+          null
+        }
+        codegenFunc
+      }
+      case ProjectExec(projectList, childPlan) => {
+        val unsafeProjection = UnsafeProjection.create(projectList, childPlan.output)
+        def projectFunc(row: InternalRow): InternalRow = {
+          unsafeProjection.apply(row)
+        }
+        projectFunc
+      }
+
+      case unknown => throw new RuntimeException(s"Unrecognized stage in codegen: ${unknown.getClass}")
+    }
+
+    (func, df.schema)
   }
 }
