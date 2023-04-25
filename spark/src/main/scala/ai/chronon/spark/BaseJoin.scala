972ed28fea9fbd73af65abb2a20b130829e7da62
Pengyu <3771747+better365@users.noreply.github.com>
2023-04-25 15:10:41 -0700

    reformat

--- spark/src/main/scala/ai/chronon/spark/BaseJoin.scala
+++ spark/src/main/scala/ai/chronon/spark/BaseJoin.scala
@@ -210,7 +210,7 @@ abstract class BaseJoin(joinConf: api.Join, endPartition: String, tableUtils: Ta
       }
       .getOrElse(leftDf)
 
-     lazy val keyMapping = Option(joinPart.keyMapping)
+    lazy val keyMapping = Option(joinPart.keyMapping)
       .map(_.asScala)
       .getOrElse(Map.empty[String, String])
 
