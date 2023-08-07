5822a5a188b63c0021305eca062b12cebab1131e
Sophie Wang <sophie.wang@airbnb.com>
2023-08-07 16:20:15 -0700

    remove todo

--- spark/src/main/scala/ai/chronon/spark/TableUtils.scala
+++ spark/src/main/scala/ai/chronon/spark/TableUtils.scala
@@ -23,7 +23,6 @@ case class TableUtils(sparkSession: SparkSession) {
   private val partitionFormat: String =
     sparkSession.conf.get("spark.chronon.partition.format", "yyyy-MM-dd")
   val partitionSpec: PartitionSpec = PartitionSpec(partitionFormat, WindowUtils.Day.millis)
-  //TODO: flip to true making it default
   val backfillValidationEnforced = sparkSession.conf.get("spark.chronon.backfill.validation.enabled", "true").toBoolean
 
   sparkSession.sparkContext.setLogLevel("ERROR")
