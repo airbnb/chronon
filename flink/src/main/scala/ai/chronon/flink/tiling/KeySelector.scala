28aa0ab6e315334ebd1f808300082588d8f8af68
Caio Camatta <caiocamatta@stripe.com>
2023-11-29 14:25:35 -0500

    Comments

--- flink/src/main/scala/ai/chronon/flink/tiling/KeySelector.scala
+++ flink/src/main/scala/ai/chronon/flink/tiling/KeySelector.scala
@@ -1,8 +1,18 @@
 package ai.chronon.flink.tiling
 
 import ai.chronon.online.GroupByServingInfoParsed
+import scala.jdk.CollectionConverters._
 
-class KeySelector {
+// TODO: add comment on what a key selector is
+object KeySelector {
+  /**
+   * Given a GroupBy, create a function to key the output of a SparkExprEval operator by the entities defined in the
+   * GroupBy. The function returns a List of size equal to the number of keys in the GroupBy.
+   *
+   * For example, if our GroupBy is "GroupBy(..., keys=["color", "size"], ...), the function will key the Flink
+   * SparkExprEval DataStream by color and size, so all events with the same (color, size) are sent to the same
+   * operator.
+   */
   protected[tiling] def getKeySelectionFunction(
                                                  groupByServingInfoParsed: GroupByServingInfoParsed
                                                ): Map[String, Any] => List[Any] = {
