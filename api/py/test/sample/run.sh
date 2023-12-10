3a907b1f90bf867eede93aaf1ab47da2152684f7
Cristian Figueroa <cristian.figueroa@airbnb.com>
2023-12-09 23:20:40 -0800

    Local

--- api/py/test/sample/run.sh
+++ api/py/test/sample/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-spark-shell -i scripts/data-loader.scala
+spark-shell -i scripts/data-loader.scala --master local[*]
 compile.py --conf joins/quickstart/training_set.py
 run.py --conf production/joins/quickstart/training_set.v1
