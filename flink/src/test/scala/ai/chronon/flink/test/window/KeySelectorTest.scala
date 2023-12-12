213cb7b202a21551d24364be5ed846a0c97a94c2
Caio Camatta <caiocamatta@stripe.com>
2023-12-12 14:55:07 -0500

    Tweak strings in key selector test

--- flink/src/test/scala/ai/chronon/flink/test/window/KeySelectorTest.scala
+++ flink/src/test/scala/ai/chronon/flink/test/window/KeySelectorTest.scala
@@ -9,18 +9,18 @@ class KeySelectorTest {
   def TestChrononFlinkJobCorrectlyKeysByAGroupbysEntityKeys(): Unit = {
     // We expect something like this to come out of the SparkExprEval operator
     val sampleSparkExprEvalOutput: Map[String, Any] =
-      Map("card_number" -> 4242, "ip" -> "192.168.0.1", "merchant" -> "stripe")
+      Map("number" -> 4242, "ip" -> "192.168.0.1", "user" -> "abc")
 
-    val groupByWithOneEntityKey = Builders.GroupBy(keyColumns = Seq("card_number"))
+    val groupByWithOneEntityKey = Builders.GroupBy(keyColumns = Seq("number"))
     val keyFunctionOne = KeySelector.getKeySelectionFunction(groupByWithOneEntityKey)
     assert(
       keyFunctionOne(sampleSparkExprEvalOutput) == List(4242)
     )
 
-    val groupByWithTwoEntityKey = Builders.GroupBy(keyColumns = Seq("card_number", "merchant"))
+    val groupByWithTwoEntityKey = Builders.GroupBy(keyColumns = Seq("number", "user"))
     val keyFunctionTwo = KeySelector.getKeySelectionFunction(groupByWithTwoEntityKey)
     assert(
-      keyFunctionTwo(sampleSparkExprEvalOutput) == List(4242, "stripe")
+      keyFunctionTwo(sampleSparkExprEvalOutput) == List(4242, "abc")
     )
   }
 
@@ -29,10 +29,10 @@ class KeySelectorTest {
     // This is more of a sanity check. It's not comprehensive.
     // SINGLE ENTITY KEY
     val map1: Map[String, Any] =
-    Map("card_number" -> 4242, "ip" -> "192.168.0.1", "merchant" -> "stripe")
+      Map("number" -> 4242, "ip" -> "192.168.0.1", "user" -> "abc")
     val map2: Map[String, Any] =
-      Map("card_number" -> 4242, "ip" -> "10.0.0.1", "merchant" -> "notstripe")
-    val groupBySingleKey = Builders.GroupBy(keyColumns = Seq("card_number"))
+      Map("number" -> 4242, "ip" -> "10.0.0.1", "user" -> "notabc")
+    val groupBySingleKey = Builders.GroupBy(keyColumns = Seq("number"))
     val keyFunctionOne = KeySelector.getKeySelectionFunction(groupBySingleKey)
     assert(
       keyFunctionOne(map1).hashCode() == keyFunctionOne(map2).hashCode()
@@ -40,19 +40,19 @@ class KeySelectorTest {
 
     // TWO ENTITY KEYS
     val map3: Map[String, Any] =
-      Map("card_number" -> 4242, "ip" -> "192.168.0.1", "merchant" -> "stripe")
+      Map("number" -> 4242, "ip" -> "192.168.0.1", "user" -> "abc")
     val map4: Map[String, Any] =
-      Map("ip" -> "192.168.0.1", "card_number" -> 4242, "merchant" -> "notstripe")
-    val groupByTwoKeys = Builders.GroupBy(keyColumns = Seq("card_number", "ip"))
+      Map("ip" -> "192.168.0.1", "number" -> 4242, "user" -> "notabc")
+    val groupByTwoKeys = Builders.GroupBy(keyColumns = Seq("number", "ip"))
     val keyFunctionTwo = KeySelector.getKeySelectionFunction(groupByTwoKeys)
     assert(
       keyFunctionTwo(map3).hashCode() == keyFunctionTwo(map4).hashCode()
     )
 
     val map5: Map[String, Any] =
-      Map("ip" -> "192.168.0.1", "card_number" -> null)
+      Map("ip" -> "192.168.0.1", "number" -> null)
     val map6: Map[String, Any] =
-      Map("ip" -> "192.168.0.1", "card_number" -> null)
+      Map("ip" -> "192.168.0.1", "number" -> null)
     assert(keyFunctionTwo(map5).hashCode() == keyFunctionTwo(map6).hashCode())
   }
 }
