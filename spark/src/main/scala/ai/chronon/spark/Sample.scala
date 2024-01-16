f11f9ba8b06658f5b08825b23acbe1baf0bdbb6e
Varant Zanoyan <vzanoyan@gmail.com>
2024-01-16 12:05:51 -0800

    WIP

--- spark/src/main/scala/ai/chronon/spark/Sample.scala
+++ spark/src/main/scala/ai/chronon/spark/Sample.scala
@@ -1,19 +1,23 @@
 package ai.chronon.spark
 
+import scala.jdk.CollectionConverters.asScalaBufferConverter
+
 import ai.chronon.api
+import ai.chronon.api.Extensions.GroupByOps
 
 class Sample(tableUtils: TableUtils) {
 
-  /*
-  def getSourceRanges(groupBy: api.GroupBy): Seq[(String, PartitionRange)] = {
+  def getSourceRanges(groupBy: api.GroupBy, ds: String): Seq[(String, PartitionRange)] = {
+    val groupByOps = new GroupByOps(groupBy)
     groupBy.sources.asScala.map{ source =>
       val range: PartitionRange = GroupBy.getIntersectedRange(
         source,
-        PartitionRange("", "")(tableUtils),
-        tableUtils)
+        PartitionRange(ds, ds)(tableUtils),
+        tableUtils,
+        groupByOps.maxWindow)
+
+      
     }
 
-
   }
-   */
 }
