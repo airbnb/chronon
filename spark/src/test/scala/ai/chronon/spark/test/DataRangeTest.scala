cac7acce90b32c11b7ebc7258c0002b269884c7a
Cam Weston <camweston@stripe.com>
2024-04-02 14:40:15 -0700

    Update date range test (#213)
    * Update date range test
    
    * Fix broken test
    
    * rename namespace

--- spark/src/test/scala/ai/chronon/spark/test/DataRangeTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/DataRangeTest.scala
@@ -1,7 +1,10 @@
 package ai.chronon.spark.test
 
+import ai.chronon.aggregator.test.Column
+import ai.chronon.api
 import ai.chronon.api.{Builders, Constants, QueryUtils, Source}
 import ai.chronon.api.Builders.Query
+import ai.chronon.spark.Extensions._
 import ai.chronon.spark.{PartitionRange, SparkSessionBuilder, TableUtils}
 import org.apache.spark.sql.SparkSession
 import org.junit.Assert.assertEquals
@@ -14,6 +17,20 @@ class DataRangeTest {
 
   @Test
   def testGenScanQueryBasedOnTime(): Unit = {
+    val namespace = "date_range_test_namespace"
+    spark.sql(s"CREATE DATABASE IF NOT EXISTS $namespace")
+    val testTable = s"$namespace.test_table"
+
+    val viewsSchema = List(
+      Column("col_1", api.StringType, 1),
+      Column("col_2", api.StringType, 1),
+    )
+
+    DataFrameGen
+      .events(spark, viewsSchema, count = 1000, partitions = 200)
+      .drop("ds")
+      .save(testTable, partitionColumns = Seq())
+
     val partitionRange: PartitionRange = PartitionRange("2024-03-01", "2024-04-01")(tableUtils)
 
     val source: Source = Builders.Source.events(
@@ -22,21 +39,22 @@ class DataRangeTest {
         wheres = Seq("col_1 = 'TEST'"),
         timeColumn = "ts"
       ),
-      table = "test_table",
+      table = testTable,
     )
 
 
     val result: String = partitionRange.genScanQueryBasedOnTime(
       source.getEvents.query,
-      "test_table",
+      testTable,
       Seq(Constants.TimeColumn -> Option(source.getEvents.query).map(_.timeColumn).orNull).toMap
     )
 
 
     val expected: String = QueryUtils.build(
         selects = Builders.Selects("ts", "col_1", "col_2"),
-        from = "test_table",
+        from = testTable,
         wheres = Seq("col_1 = 'TEST'", "ts >= 1709251200000", "ts < 1712016000000"),
+        isLocalized = false
       )
 
     assertEquals(expected.stripMargin, result.stripMargin)
