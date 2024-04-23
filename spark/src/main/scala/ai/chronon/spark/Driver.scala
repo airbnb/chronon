27a52763f253bda8998583c1ddf583026022e7d0
yuli_han <yuli_han@yuli-hans-MacBook-Pro.local>
2024-04-23 11:53:17 -0700

    fix bug

--- spark/src/main/scala/ai/chronon/spark/Driver.scala
+++ spark/src/main/scala/ai/chronon/spark/Driver.scala
@@ -725,12 +725,12 @@ object Driver {
     def run(args: Args): Unit = {
       val putRequest = args.metaDataStore.putConfByName(args.confPath())
       val res = Await.result(putRequest, 1.hour)
-      logger.info(
-        s"Uploaded Chronon ${args.confPath} Configs to the KV store, success count = ${res.count(v => v)}, failure count = ${res.count(!_)}")
+      logger.info(s"Uploaded Chronon ${args.confPath} Configs to the KV store, success count = ${res.count(v =>
+        v)}, failure count = ${res.count(!_)}")
       val putByTeamRequest = args.metaDataStore.putConfByTeam(args.confPath())
       val resByTeam = Await.result(putByTeamRequest, 1.hour)
-      logger.info(
-        s"Uploaded Chronon ${args.confPath} entity by team to the KV store, success count = ${resByTeam.count(v => v)}, failure count = ${resByTeam.count(!_)}")
+      logger.info(s"Uploaded Chronon ${args.confPath} entity by team to the KV store, success count = ${resByTeam.count(
+        v => v)}, failure count = ${resByTeam.count(!_)}")
     }
   }
 
