a0fc37780aee78653e6d65bbadd4907e2ceb50a9
divyamanohar-stripe <111152273+divyamanohar-stripe@users.noreply.github.com>
2024-04-29 09:29:15 -0700

    re-run
    Signed-off-by: divyamanohar-stripe <111152273+divyamanohar-stripe@users.noreply.github.com>

--- online/src/main/java/ai/chronon/online/FlagStore.java
+++ online/src/main/java/ai/chronon/online/FlagStore.java
@@ -3,7 +3,7 @@ package ai.chronon.online;
 import java.io.Serializable;
 import java.util.Map;
 
-// Interface to allow rolling out features/infrastructure changes in a safe & controlled manner
+// Interface to allow rolling out features/infrastructure changes in a safe, controlled manner
 public interface FlagStore extends Serializable {
     Boolean isSet(String flagName, Map<String, String> attributes);
 }
