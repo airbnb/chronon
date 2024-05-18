32a90d6ad39843598b37054a057b55161d4f9a09
nikhilsimha <r.nikhilsimha@gmail.com>
2024-05-18 12:49:12 -0700

    Add log4j properties to resources

--- spark/src/main/scala/ai/chronon/spark/SparkSessionBuilder.scala
+++ spark/src/main/scala/ai/chronon/spark/SparkSessionBuilder.scala
@@ -91,7 +91,7 @@ object SparkSessionBuilder {
     }
     val spark = builder.getOrCreate()
     // disable log spam
-    // spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel("ERROR")
 
     Logger.getLogger("parquet.hadoop").setLevel(java.util.logging.Level.SEVERE)
     spark
@@ -119,7 +119,7 @@ object SparkSessionBuilder {
     }
     val spark = builder.getOrCreate()
     // disable log spam
-    // spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel("ERROR")
     Logger.getLogger("parquet.hadoop").setLevel(java.util.logging.Level.SEVERE)
     spark
   }
