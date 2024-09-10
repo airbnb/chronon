5503c403f980fa68968a02102d2b5a65261d3562
yuli-han <31667422+yuli-han@users.noreply.github.com>
2024-09-10 11:04:11 -0700

    Update the online derivation validation behavior (#825)
    * add keys back in compiler
    
    * add test cases
    
    ---------
    
    Co-authored-by: yuli_han <yuli_han@yuli-hans-MacBook-Pro.local>

--- api/py/test/sample/group_bys/sample_team/sample_group_by_with_derivations.py
+++ api/py/test/sample/group_bys/sample_team/sample_group_by_with_derivations.py
@@ -12,15 +12,8 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+from ai.chronon.group_by import Aggregation, Derivation, GroupBy, Operation
 from sources import test_sources
-from ai.chronon.group_by import (
-    GroupBy,
-    Aggregation,
-    Operation,
-    Window,
-    TimeUnit,
-    Derivation
-)
 
 v1 = GroupBy(
     sources=test_sources.staging_entities,
@@ -30,15 +23,14 @@ v1 = GroupBy(
         Aggregation(input_column="viewed_unique_count", operation=Operation.SUM),
     ],
     production=False,
-    table_properties={
-        "sample_config_json": """{"sample_key": "sample_value"}""",
-        "description": "sample description"
-    },
+    table_properties={"sample_config_json": """{"sample_key": "sample_value"}""", "description": "sample description"},
     derivations=[
         Derivation(
-            name="impressed_unique_count_1d_new_name",
-            expression="impressed_unique_count_sum"
+            name=column_name,
+            expression=column_name,
         )
-    ],
+        for column_name in ["ds", "s2CellId", "place_id"]
+    ]
+    + [Derivation(name="impressed_unique_count_1d_new_name", expression="impressed_unique_count_sum")],
     output_namespace="sample_namespace",
 )
--- api/py/test/sample/joins/sample_team/sample_join_derivation.py
+++ api/py/test/sample/joins/sample_team/sample_join_derivation.py
@@ -16,36 +16,31 @@ Sample Join
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-from sources import test_sources
+from ai.chronon.join import Derivation, Join, JoinPart
 from group_bys.sample_team import (
-    event_sample_group_by,
     entity_sample_group_by_from_module,
-    group_by_with_kwargs,
+    event_sample_group_by,
 )
-
-from ai.chronon.join import Join, JoinPart, Derivation
-
+from sources import test_sources
 
 v1 = Join(
     left=test_sources.event_source,
     right_parts=[
         JoinPart(
             group_by=event_sample_group_by.v1,
-            key_mapping={'subject': 'group_by_subject'},
+            key_mapping={"subject": "group_by_subject"},
         ),
         JoinPart(
             group_by=entity_sample_group_by_from_module.v1,
-            key_mapping={'subject': 'group_by_subject'},
-        )
+            key_mapping={"subject": "group_by_subject"},
+        ),
     ],
     derivations=[
         Derivation(
             name="derived_field",
-            expression="sample_team_event_sample_group_by_v1_event_sum_7d / sample_team_entity_sample_group_by_from_module_v1_entity_last"
+            expression="sample_team_event_sample_group_by_v1_event_sum_7d / sample_team_entity_sample_group_by_from_module_v1_entity_last",
         ),
-        Derivation(
-            name="*",
-            expression="*"
-        )
-    ]
+        Derivation(name="group_by_subject", expression="group_by_subject"),
+        Derivation(name="ds", expression="ds"),
+    ],
 )
