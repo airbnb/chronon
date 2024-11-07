eea7069bb711dffc6f8b9f772ae7dd4163fd983f
yuli_han <yuli_han@yuli-hans-MacBook-Pro.local>
2024-11-07 14:44:39 -0800

    fix syntax

--- spark/src/test/scala/ai/chronon/spark/test/GroupByTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/GroupByTest.scala
@@ -19,8 +19,7 @@ package ai.chronon.spark.test
 import ai.chronon.aggregator.test.{CStream, Column, NaiveAggregator}
 import ai.chronon.aggregator.windowing.FiveMinuteResolution
 import ai.chronon.api.Extensions._
-import ai.chronon.api.{Aggregation, Builders, Constants, DoubleType, IntType, LongType, Operation, Source, StringType, TimeUnit, Window}
-import ai.chronon.api.Builders.Derivation
+import ai.chronon.api.{Aggregation, Builders, Constants, Derivation, DoubleType, IntType, LongType, Operation, Source, StringType, TimeUnit, Window}
 import ai.chronon.online.{RowWrapper, SparkConversions}
 import ai.chronon.spark.Extensions._
 import ai.chronon.spark._
