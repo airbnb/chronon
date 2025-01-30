667f1c945cbb09773820f33ce9c6eea801f78f92
Xiaohui Sun <sunxiaohui.bj@gmail.com>
2025-01-29 16:31:23 -0800

    Set team name for inline modules (#913)
    * Set team name for inline modules
    
    * explictly set team name for inline group_bys
    
    * only check inline_group_bys when parsing joins
    
    * add unit test
    
    ---------
    
    Co-authored-by: xiaohui sun <xiaohui_sun@xiaohuis-MacBook-Pro.local>
    Co-authored-by: xiaohui sun <xiaohui_sun@xiaohuis-MBP.attlocal.net>

--- api/py/ai/chronon/repo/extract_objects.py
+++ api/py/ai/chronon/repo/extract_objects.py
@@ -1,4 +1,3 @@
-
 #     Copyright (C) 2023 The Chronon Authors.
 #
 #     Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,23 +18,19 @@ import importlib.util
 import logging
 import os
 
+from ai.chronon.api.ttypes import GroupBy, Join
 from ai.chronon.logger import get_logger
 
 
-def from_folder(root_path: str,
-                full_path: str,
-                cls: type,
-                log_level=logging.INFO):
+def from_folder(root_path: str, full_path: str, cls: type, log_level=logging.INFO):
     """
     Recursively consumes a folder, and constructs a map
     Creates a map of object qualifier to
     """
-    if full_path.endswith('/'):
+    if full_path.endswith("/"):
         full_path = full_path[:-1]
 
-    python_files = glob.glob(
-        os.path.join(full_path, "**/*.py"),
-        recursive=True)
+    python_files = glob.glob(os.path.join(full_path, "**/*.py"), recursive=True)
     result = {}
     for f in python_files:
         try:
@@ -46,7 +41,7 @@ def from_folder(root_path: str,
     return result
 
 
-def import_module_set_name(module, cls):
+def import_module_set_name(module, cls, inline_group_bys=None):
     """evaluate imported modules to assign object name"""
     for name, obj in list(module.__dict__.items()):
         if isinstance(obj, cls):
@@ -56,26 +51,31 @@ def import_module_set_name(module, cls):
             # obj.metaData.team=user
             obj.metaData.name = module.__name__.partition(".")[2] + "." + name
             obj.metaData.team = module.__name__.split(".")[1]
+            if isinstance(obj, Join) and inline_group_bys:
+                for jp in obj.joinParts:
+                    if jp.groupBy in inline_group_bys:
+                        jp.groupBy.metaData.team = obj.metaData.team
+
     return module
 
 
-def from_file(root_path: str,
-              file_path: str,
-              cls: type,
-              log_level=logging.INFO):
+def from_file(root_path: str, file_path: str, cls: type, log_level=logging.INFO):
     logger = get_logger(log_level)
-    logger.debug(
-        "Loading objects of type {cls} from {file_path}".format(**locals()))
+    logger.debug("Loading objects of type {cls} from {file_path}".format(**locals()))
     # mod_qualifier includes team name and python script name without `.py`
     # this line takes the full file path as input, strips the root path on the left side
     # strips `.py` on the right side and finally replaces the slash sign to dot
     # eg: the output would be `team_name.python_script_name`
-    mod_qualifier = file_path[len(root_path.rstrip('/')) + 1:-3].replace("/", ".")
+    mod_qualifier = file_path[len(root_path.rstrip("/")) + 1 : -3].replace("/", ".")
     mod = importlib.import_module(mod_qualifier)
+
+    # get inline group_bys
+    inline_group_bys = [k for k in mod.__dict__.values() if isinstance(k, GroupBy)] if cls == Join else None
 
     # the key of result dict would be `team_name.python_script_name.[group_by_name|join_name|staging_query_name]`
     # real world case: psx.reservation_status.v1
-    import_module_set_name(mod, cls)
+    import_module_set_name(mod, cls, inline_group_bys)
+
     result = {}
 
     for obj in [o for o in mod.__dict__.values() if isinstance(o, cls)]:
