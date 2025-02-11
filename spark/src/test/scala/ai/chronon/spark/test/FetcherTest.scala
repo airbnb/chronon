a23e4f78efefc5dc4e25f1c7b5154c57c38c7c23
xiaohui sun <xiaohui_sun@xiaohuis-MacBook-Pro.local>
2025-02-11 08:33:19 -0800

    fix unit tests

--- spark/src/test/scala/ai/chronon/spark/test/FetcherTest.scala
+++ spark/src/test/scala/ai/chronon/spark/test/FetcherTest.scala
@@ -725,12 +725,7 @@ object FetcherTestUtil {
       val result = requests.iterator
         .grouped(chunkSize)
         .map { oldReqs =>
-          // deliberately mis-type a few keys
           val r = oldReqs
-            .map(r =>
-              r.copy(keys = r.keys.mapValues { v =>
-                if (v.isInstanceOf[java.lang.Long]) v.toString else v
-              }.toMap))
           val responses = if (useJavaFetcher) {
             // Converting to java request and using the toScalaRequest functionality to test conversion
             val convertedJavaRequests = r.map(new JavaRequest(_)).toJava
@@ -747,10 +742,7 @@ object FetcherTestUtil {
             fetcher.fetchJoin(r)
           }
 
-          // fix mis-typed keys in the request
-          val fixedResponses =
-            responses.map(resps => resps.zip(oldReqs).map { case (resp, req) => resp.copy(request = req) })
-          System.currentTimeMillis() -> fixedResponses
+          System.currentTimeMillis() -> responses
         }
         .flatMap {
           case (start, future) =>
