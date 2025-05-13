1012ee31303f109f9ce1cf9867cf4447c9b102a6
Abby Whittier <abbywhittier309@gmail.com>
2025-05-12 23:07:47 -0400

    Update spark/src/main/scala/ai/chronon/spark/SparkSessionBuilder.scala
    Co-authored-by: Pengyu Hou <3771747+pengyu-hou@users.noreply.github.com>
    Signed-off-by: Abby Whittier <abbywhittier309@gmail.com>

--- spark/src/main/scala/ai/chronon/spark/SparkSessionBuilder.scala
+++ spark/src/main/scala/ai/chronon/spark/SparkSessionBuilder.scala
@@ -98,6 +98,16 @@ object SparkSessionBuilder {
       // use all threads - or the tests will be slow
         .master("local[*]")
         .config("spark.kryo.registrationRequired", s"${localWarehouseLocation.isEmpty}")
+      // ── use an in‐memory Derby metastore rather than disk + BoneCP ──
+        .config("spark.hadoop.javax.jdo.option.ConnectionURL", "jdbc:derby:memory:metastore_db;create=true")
+        .config("spark.hadoop.javax.jdo.option.ConnectionDriverName", "org.apache.derby.jdbc.EmbeddedDriver")
+        .config("spark.hadoop.javax.jdo.option.ConnectionUserName", "APP")
+        .config("spark.hadoop.javax.jdo.option.ConnectionPassword", "APP")
+        .config("spark.ui.enabled", "false")
+        .config("spark.chronon.outputParallelismOverride", "2")
+        .config("spark.chronon.group_by.parallelism", "2")
+        .config("spark.sql.shuffle.partitions", "2")
+        .config("spark.default.parallelism", "2")
         .config("spark.testing", "true")
         .config("spark.ui.enabled", false)
         .config("spark.sql.adaptive.enabled", true)
