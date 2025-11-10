357bd884e076a631508487492005e3fab908efd4
Yogesh Patikkal Veettil <ypatikkalveettil@roku.com>
2025-11-09 20:40:17 -0800

    update assert stmt - dummy to run pipeline

--- spark/src/main/scala/ai/chronon/spark/GroupBy.scala
+++ spark/src/main/scala/ai/chronon/spark/GroupBy.scala
@@ -666,7 +666,7 @@ object GroupBy {
           assert(
             queryRange.isSingleDay,
             s"ChrononRunDs is only supported for single day queries. " +
-              s"Got start: ${queryRange.start}, end: ${queryRange.end} (range spans multiple days)"
+              s"Got start: ${queryRange.start}, end: ${queryRange.end} (date range include multiple days)"
           )
           val parametricMacro = ParametricMacro(Constants.ChrononRunDs, _ => s"'${queryRange.start}'")
           (keyValue._1, parametricMacro.replace(keyValue._2))
