ac55de9622bb70257634513fdf92aafe4c7bfa7e
Yogesh <yogeshpv@users.noreply.github.com>
2025-11-19 09:10:04 -0800

    Update ChrononRunDS assert criteria (#1063)
    * update chronon run ds criteria
    
    * scalafmt fix
    
    Signed-off-by: Yogesh <yogeshpv@users.noreply.github.com>
    
    * update style
    
    * update assert stmt - dummy to run pipeline
    
    ---------
    
    Signed-off-by: Yogesh <yogeshpv@users.noreply.github.com>
    Co-authored-by: Yogesh Patikkal Veettil <ypatikkalveettil@roku.com>
    Co-authored-by: Pengyu Hou <3771747+pengyu-hou@users.noreply.github.com>

--- spark/src/main/scala/ai/chronon/spark/GroupBy.scala
+++ spark/src/main/scala/ai/chronon/spark/GroupBy.scala
@@ -663,9 +663,12 @@ object GroupBy {
     val selects = Option(source.query.selects)
       .map(_.toScala.map(keyValue => {
         if (keyValue._2.contains(Constants.ChrononRunDs)) {
-          assert(intersectedRange.isDefined && intersectedRange.get.isSingleDay,
-                 s"ChrononRunDs is only supported for single day queries")
-          val parametricMacro = ParametricMacro(Constants.ChrononRunDs, _ => queryRange.start)
+          assert(
+            queryRange.isSingleDay,
+            s"ChrononRunDs is only supported for single day queries. " +
+              s"Got start: ${queryRange.start}, end: ${queryRange.end} (date range include multiple days)"
+          )
+          val parametricMacro = ParametricMacro(Constants.ChrononRunDs, _ => s"'${queryRange.start}'")
           (keyValue._1, parametricMacro.replace(keyValue._2))
         } else {
           keyValue
