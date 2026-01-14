ec0a88353c9b78ec439a9d42a484978b7c4077f1
Pengyu Hou <3771747+pengyu-hou@users.noreply.github.com>
2026-01-14 15:35:43 -0600

    Revert "Update ChrononRunDS assert criteria (#1063)"
    This reverts commit ac55de9622bb70257634513fdf92aafe4c7bfa7e.

--- spark/src/main/scala/ai/chronon/spark/GroupBy.scala
+++ spark/src/main/scala/ai/chronon/spark/GroupBy.scala
@@ -663,12 +663,9 @@ object GroupBy {
     val selects = Option(source.query.selects)
       .map(_.toScala.map(keyValue => {
         if (keyValue._2.contains(Constants.ChrononRunDs)) {
-          assert(
-            queryRange.isSingleDay,
-            s"ChrononRunDs is only supported for single day queries. " +
-              s"Got start: ${queryRange.start}, end: ${queryRange.end} (date range include multiple days)"
-          )
-          val parametricMacro = ParametricMacro(Constants.ChrononRunDs, _ => s"'${queryRange.start}'")
+          assert(intersectedRange.isDefined && intersectedRange.get.isSingleDay,
+                 s"ChrononRunDs is only supported for single day queries")
+          val parametricMacro = ParametricMacro(Constants.ChrononRunDs, _ => queryRange.start)
           (keyValue._1, parametricMacro.replace(keyValue._2))
         } else {
           keyValue
