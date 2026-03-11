363d475f85a8f8fda32618643c31a397e99d570e
yuli-han <31667422+yuli-han@users.noreply.github.com>
2026-03-11 14:41:59 -0700

    Update spark/src/main/scala/ai/chronon/spark/streaming/PushModeConfig.scala
    Co-authored-by: Pengyu Hou <3771747+pengyu-hou@users.noreply.github.com>
    Signed-off-by: yuli-han <31667422+yuli-han@users.noreply.github.com>

--- spark/src/main/scala/ai/chronon/spark/streaming/PushModeConfig.scala
+++ spark/src/main/scala/ai/chronon/spark/streaming/PushModeConfig.scala
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2023 The Chronon Authors.
+ *    Copyright (C) 2026 The Chronon Authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
